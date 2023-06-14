066acd4391f42daacb7af7effc5e5b9b7d9c4c77
Dorin Michaeli <dorin.michaeli1@gmail.com>
2023-06-14 04:56:38 +0300

    Renamed request param from "id" to "markerId".

--- ws/events/deleteMarker.event.js
+++ ws/events/deleteMarker.event.js
@@ -4,27 +4,28 @@ import { serializeMessage } from '../core/serialize.js';
 import { broadcastMessageToGroup } from '../core/broadcast.js';
 
 export async function handleDeleteMarker(req, { wss, markerModel }) {
-  const { id } = req.data;
+  const { markerId } = req.data;
   // Validate the id sent by the client.
-  if (typeof id !== 'string') {
+  if (typeof markerId !== 'string') {
     throw new Error('Cannot delete marker, missing or invalid marker id.');
   }
 
   // Delete the marker.
-  const deletedMarker = await deleteMarker(id, req.socket.userInfo.email, { markerModel });
+  const deletedMarker = await deleteMarker(markerId, req.socket.userInfo.email, { markerModel });
 
   if (!deletedMarker) {
     // Let the user know if the marker could not be deleted.
-    throw new Error(`Cannot delete marker, either marker with id ${id} does not exist, or you don't own it.`);
+    throw new Error(`Cannot delete marker, either marker with id ${markerId} does not exist, or you don't own it.`);
   }
 
   // Serialize the message.
   const outgoingMessage = serializeMessage(SERVER_MESSAGE_TYPE.MARKER_DELETED, {
     // Send the id of the deleted marker to the clients,
     // so they can remove it from the map.
-    id: id,
+    id: markerId,
     // Send some of the info about the now-deleted
     // marker to the clients for convenience.
+    groupId: deletedMarker.groupId,
     owner: deletedMarker.owner,
     lat: deletedMarker.lat,
     lon: deletedMarker.lon,
